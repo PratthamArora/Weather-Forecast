19118be4ac4c0c33838b1c231fd4128d3110d005
PratthamArora <arora.prattham@gmail.com>
2020-04-05 00:36:02 +0530

    added wind speed , humidity, max-temp

--- lib/ui/mid_view.dart
+++ lib/ui/mid_view.dart
@@ -4,7 +4,6 @@ import 'package:weatherme/model/weather_model.dart';
 import 'package:weatherme/utils/Utility.dart';
 
 Widget midView(AsyncSnapshot<WeatherModel> snapshot) {
-  var forecastList = snapshot.data.main;
   var cityName = snapshot.data.name;
   var countryName = snapshot.data.sys.country;
   Container midView = Container(
@@ -46,12 +45,69 @@ Widget midView(AsyncSnapshot<WeatherModel> snapshot) {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  '${forecastList.temp.toStringAsFixed(0)}°C',
+                  '${snapshot.data.main.temp.toStringAsFixed(0)}°C',
                   style: TextStyle(
                     fontSize: 34.0,
                   ),
                 ),
+                SizedBox(
+                  width: 8.0,
+                ),
                 Text('${snapshot.data.weather[0].description.toUpperCase()}'),
+              ],
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                          '${snapshot.data.wind.speed.toStringAsFixed(1)} Km/h'),
+                      Icon(
+                        Icons.arrow_forward,
+                        size: 20.0,
+                        color: Colors.brown,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                          '${snapshot.data.main.humidity.toStringAsFixed(0)} %'),
+                      Icon(
+                        Icons.hot_tub,
+                        size: 20.0,
+                        color: Colors.brown,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                          '${snapshot.data.main.tempMax.toStringAsFixed(1)}°C'),
+                      Icon(
+                        Icons.wb_sunny,
+                        size: 20.0,
+                        color: Colors.brown,
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
